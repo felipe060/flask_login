75dd050865983a26b17962e37431cf5738902c12
felipe060 <felipeflip29@hotmail.com>
2023-05-17 08:36:59 -0300

    2º -->

--- index.py
+++ index.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def error_404(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
